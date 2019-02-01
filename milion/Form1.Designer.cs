e54bc099bffa104068211e614f668f22acf7c9e4
236824 <44272403+236824@users.noreply.github.com>
2019-02-01 13:36:55 +0100

    Add files via upload

---
+++ milion/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace milion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.A_btt = new System.Windows.Forms.RadioButton();
+            this.B_btt = new System.Windows.Forms.RadioButton();
+            this.C_btt = new System.Windows.Forms.RadioButton();
+            this.D_btt = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(87, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(700, 206);
+            this.textBox1.TabIndex = 1;
+            // 
+            // A_btt
+            // 
+            this.A_btt.AutoSize = true;
+            this.A_btt.Location = new System.Drawing.Point(87, 285);
+            this.A_btt.Name = "A_btt";
+            this.A_btt.Size = new System.Drawing.Size(14, 13);
+            this.A_btt.TabIndex = 2;
+            this.A_btt.TabStop = true;
+            this.A_btt.UseVisualStyleBackColor = true;
+            this.A_btt.CheckedChanged += new System.EventHandler(this.A_btt_CheckedChanged);
+            // 
+            // B_btt
+            // 
+            this.B_btt.AutoSize = true;
+            this.B_btt.Location = new System.Drawing.Point(87, 323);
+            this.B_btt.Name = "B_btt";
+            this.B_btt.Size = new System.Drawing.Size(14, 13);
+            this.B_btt.TabIndex = 3;
+            this.B_btt.TabStop = true;
+            this.B_btt.UseVisualStyleBackColor = true;
+            this.B_btt.CheckedChanged += new System.EventHandler(this.B_btt_CheckedChanged);
+            // 
+            // C_btt
+            // 
+            this.C_btt.AutoSize = true;
+            this.C_btt.Location = new System.Drawing.Point(87, 362);
+            this.C_btt.Name = "C_btt";
+            this.C_btt.Size = new System.Drawing.Size(14, 13);
+            this.C_btt.TabIndex = 4;
+            this.C_btt.TabStop = true;
+            this.C_btt.UseVisualStyleBackColor = true;
+            this.C_btt.CheckedChanged += new System.EventHandler(this.C_btt_CheckedChanged);
+            // 
+            // D_btt
+            // 
+            this.D_btt.AutoSize = true;
+            this.D_btt.Location = new System.Drawing.Point(87, 405);
+            this.D_btt.Name = "D_btt";
+            this.D_btt.Size = new System.Drawing.Size(14, 13);
+            this.D_btt.TabIndex = 5;
+            this.D_btt.TabStop = true;
+            this.D_btt.UseVisualStyleBackColor = true;
+            this.D_btt.CheckedChanged += new System.EventHandler(this.D_btt_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(645, 451);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 44);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Następne";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::milion.Properties.Resources.maxresdefaultdd;
+            this.ClientSize = new System.Drawing.Size(891, 519);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.D_btt);
+            this.Controls.Add(this.C_btt);
+            this.Controls.Add(this.B_btt);
+            this.Controls.Add(this.A_btt);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Name = "Form1";
+            this.Text = "Milion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton A_btt;
+        private System.Windows.Forms.RadioButton B_btt;
+        private System.Windows.Forms.RadioButton C_btt;
+        private System.Windows.Forms.RadioButton D_btt;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ milion/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace milion
+{
+    public partial class Form1 : Form
+    {
+        string p;   //Pytanie
+        
+        int licznik = 0;   //licznki pyt
+        int good = 0;     //licznik dobrych
+        string a;
+        string b;
+        string c;
+        string d;
+        string o;   //odpowiedź poprawna
+        string wybrane;     //wybrana odpowiedź
+        public Form1()
+        {
+            Wczytaj();
+            
+            InitializeComponent();
+            Ustaw();
+        }
+
+        //private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void A_btt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (A_btt.Checked == true)
+            {
+                wybrane = "A";
+                //MessageBox.Show("Napewno A?");
+            }
+            else wybrane = "0";
+
+
+        }
+
+        private void B_btt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (B_btt.Checked == true)
+            {
+                wybrane = "B";
+            }
+            else wybrane = "0";
+        }
+
+        private void C_btt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (C_btt.Checked == true)
+            {
+                wybrane = "C";
+            }
+            else wybrane = "0";
+        }
+
+        private void D_btt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (D_btt.Checked == true)
+            {
+                wybrane = "D";
+            }
+            else wybrane = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Spr();         //sprawdza czy odpowiedź udzielona jest dobra
+            
+            licznik++;
+
+            BttRes();   //uncheck
+
+            Wczytaj();      //wczytuje z pliku do zmiennych
+
+            if (a != null)
+            {
+                Ustaw();    //wstawia zmienne do kontrolek
+            }
+            else
+            {
+               // Form2 frm = new Form2();
+                button1.Text = "Koniec";
+                MessageBox.Show("Koniec! Zdobyłeś " + good.ToString() + "pkt na " + licznik.ToString() + " możliwych!");
+                
+            }
+
+            if (button1.Text == "Koniec") this.Close();
+                
+            
+        }
+        
+        private /*async*/ void Wczytaj() //synchronicznie czy nie???
+        {                       
+            string plk = @"test.txt";
+            StreamReader sr = new StreamReader(plk);
+            for(int i=0; i <= licznik ; i++)
+            { 
+                p = sr.ReadLine();
+                a = sr.ReadLine();
+                b = sr.ReadLine();
+                c = sr.ReadLine();
+                d = sr.ReadLine();
+                o = sr.ReadLine();
+            }
+            sr.Close();
+            
+        }
+
+        void Ustaw()
+        {
+            textBox1.Text = p;
+            A_btt.Text = a;
+            B_btt.Text = b;
+            C_btt.Text = c;
+            D_btt.Text = d;
+        }
+
+        void Spr()
+        {
+            switch (wybrane)
+            {
+                case "A":
+                    if (o == "A") good++;
+                    break;
+
+                case "B":
+                    if (o == "B") good++;
+                    break;
+
+                case "C":
+                    if (o == "C") good++;
+                    break;
+
+                case "D":
+                    if (o == "D") good++;
+                    break;
+            }
+        }
+
+        void BttRes()
+        {
+            A_btt.Checked = false;
+            B_btt.Checked = false;
+            C_btt.Checked = false;
+            D_btt.Checked = false;
+        }
+
+        
+    }
+}
